87b0773e4709f4fcf94e68d879463988e98f9fac
ayoisaac <ayoisaac306@gmail.com>
2026-02-25 13:39:24 -0800

    fixed my file and code

--- booking web (backend)/bookingproject/bookingapp/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import submit_booking, success_page
-
-urlpatterns = [
-    path('submit-booking/', submit_booking, name='submit-booking'),
-    path('success/', success_page, name='success'),
-]
